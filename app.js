2b2c739c4d547d2459abb0cf8c6b1a8c27a689be
Alexandr Ilushechkin <progruzovik@gmail.com>
2018-11-11 19:55:34 +0300

    add animation

---
+++ app.js
@@ -0,0 +1,27 @@
+var app = new PIXI.Application();
+document.body.appendChild(app.view);
+
+app.stop();
+
+PIXI.loader
+    .add('spritesheet', 'gg.json')
+    .load(onAssetsLoaded);
+
+function onAssetsLoaded() {
+
+    // create an array to store the textures
+    var explosionTextures = [],
+        i;
+
+    for (i = 0; i < 26; i++) {
+         var texture = PIXI.Texture.fromFrame('gg' + (i + 1) + '.png');
+         explosionTextures.push(texture);
+    }
+
+    var explosion = new PIXI.extras.AnimatedSprite(explosionTextures);
+    explosion.play();
+    app.stage.addChild(explosion);
+
+    // start animating
+    app.start();
+}
